e429d65253ae9136fe7e9581767ae9b6fb92b781
NetAcad DEVASC <changeme@example.com>
2024-07-30 18:58:27 +0000

    return to default
    Signed-off-by: NetAcad DEVASC <changeme@example.com>

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,5 @@
 # Manifest that kills a process named killmenow
-# uses exec resource with pkill command
 
 exec { 'killmenow':
-  command => 'pkill killmenow',
-  path    => '/usr/bin/'
+  command => '/usr/bin/pkill -f /killmenow'
 }
