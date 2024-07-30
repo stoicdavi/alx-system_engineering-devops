d610486bd57eef05cac9811472a5791171844cb3
David Nanjila <nanjiladavid2@gmail.com>
2024-07-30 19:18:59 +0000

    fix
    Signed-off-by: David Nanjila <nanjiladavid2@gmail.com>

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,7 +3,14 @@
 #     Install flask
 #     Version must be 2.1.0
 
-package { 'flask':
+# Puppet Manifest: Install Flask from pip3
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+}
+
+package { 'Werkzeug':
+  ensure   => '2.0.0',
+  provider => 'pip3',
 }
